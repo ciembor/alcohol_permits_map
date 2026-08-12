b96e95539baa3aa3d9c372ca71f15aaea8126609
Maciej Ciemborowicz <maciej.ciemborowicz@gmail.com>
2026-08-12 02:15:32 +0200

    Prefix map URLs for subpath deployment

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,7 +4,15 @@ module ApplicationHelper
     query['locale'] = locale.to_s unless locale.to_sym == I18n.default_locale
     query_string = query.to_query
 
-    [request.path, query_string.presence].compact.join('?')
+    public_path([request.path, query_string.presence].compact.join('?'))
+  end
+
+  def public_path(path)
+    relative_root = Rails.application.config.relative_url_root.to_s.chomp('/')
+    return path if relative_root.blank?
+    return path if path == relative_root || path.start_with?("#{relative_root}/", "#{relative_root}?")
+
+    "#{relative_root}#{path}"
   end
 
   def language_urls
